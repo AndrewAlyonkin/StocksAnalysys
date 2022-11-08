a089b2ef218e10826f378a08058dd262d439c257
AlenkinAndrew <oxqq@ya.ru>
2022-11-08 09:41:50 +0300

    configure cache

--- moexStocks/src/main/java/com/afalenkin/moexStocks/feign/GovernmentBondsClient.java
+++ moexStocks/src/main/java/com/afalenkin/moexStocks/feign/GovernmentBondsClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
  * @author Alenkin Andrew
  * oxqq@ya.ru
  */
-@FeignClient(name = "corporate", url = "${moex.bonds.government}", configuration = FeignConfig.class)
+@FeignClient(name = "government", url = "${moex.bonds.government}", configuration = FeignConfig.class)
 public interface GovernmentBondsClient {
     @GetMapping
     String getBonds();
